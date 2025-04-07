5a6cbda3e75179e605eaa9a08d053fb477a3c0fb
Cosmas Ken <Cosmasarubaken@gmail.com>
2025-04-07 13:00:30 +0300

    Add Readme

--- wallet/target/debug/build/generic-array-ee889f7b4a0866eb/build_script_build-ee889f7b4a0866eb.d
+++ wallet/target/debug/build/generic-array-ee889f7b4a0866eb/build_script_build-ee889f7b4a0866eb.d
@@ -1,5 +1,5 @@
-/home/groot/Code/boss-2025/wallet/target/debug/build/generic-array-ee889f7b4a0866eb/build_script_build-ee889f7b4a0866eb: /home/groot/.cargo/registry/src/index.crates.io-6f17d22bba15001f/generic-array-0.14.7/build.rs
+/home/groot/Code/boss-2025/btc-scripts/wallet/target/debug/build/generic-array-ee889f7b4a0866eb/build_script_build-ee889f7b4a0866eb: /home/groot/.cargo/registry/src/index.crates.io-6f17d22bba15001f/generic-array-0.14.7/build.rs
 
-/home/groot/Code/boss-2025/wallet/target/debug/build/generic-array-ee889f7b4a0866eb/build_script_build-ee889f7b4a0866eb.d: /home/groot/.cargo/registry/src/index.crates.io-6f17d22bba15001f/generic-array-0.14.7/build.rs
+/home/groot/Code/boss-2025/btc-scripts/wallet/target/debug/build/generic-array-ee889f7b4a0866eb/build_script_build-ee889f7b4a0866eb.d: /home/groot/.cargo/registry/src/index.crates.io-6f17d22bba15001f/generic-array-0.14.7/build.rs
 
 /home/groot/.cargo/registry/src/index.crates.io-6f17d22bba15001f/generic-array-0.14.7/build.rs:
